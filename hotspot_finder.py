1780a6a6d767a190e34d2d37feae1b2424b3e4ea
gabriel-rmrz <gabriel-rmrz@users.noreply.github.com>
2023-10-23 15:10:20 +0200

    Adding gussian adaptative threshold."

--- hotspot_finder.py
+++ hotspot_finder.py
@@ -1,3 +1,4 @@
+DEBUG = False
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
@@ -45,34 +46,50 @@ def plot_preprocessed_img(img_cv_in):
   '''
 
 def crop_rect(img, rect):
-    # get the parameter of the small rectangle
-    center, size, angle = rect[0], rect[1], rect[2]
-    center, size = tuple(map(int, center)), tuple(map(int, size))
+  # get the parameter of the small rectangle
+  center, size, angle = rect[0], rect[1], rect[2]
+  center, size = tuple(map(int, center)), tuple(map(int, size))
 
-    # get row and col num in img
-    height, width = img.shape[0], img.shape[1]
+  # get row and col num in img
+  height, width = img.shape[0], img.shape[1]
 
-    # calculate the rotation matrix
-    M = cv.getRotationMatrix2D(center, angle, 1)
-    # rotate the original image
-    img_rot = cv.warpAffine(img, M, (width, height))
+  # calculate the rotation matrix
+  M = cv.getRotationMatrix2D(center, angle, 1)
+  # rotate the original image
+  img_rot = cv.warpAffine(img, M, (width, height))
 
-    # now rotated rectangle becomes vertical, and we crop it
-    img_crop = cv.getRectSubPix(img_rot, size, center)
+  # now rotated rectangle becomes vertical, and we crop it
+  img_crop = cv.getRectSubPix(img_rot, size, center)
 
-    return img_crop, img_rot
-def find_rectangles(img_cv_in):
+  return img_crop, img_rot
+def prepare_img(img_cv, img_cv2):
+  #img_cv = img_cv[:,:,0]
+  m1 = np.mean(img_cv)
+  m2 = np.mean(img_cv2)
+  #im_array = im_array * (m1/m2)
+  img_cv2 = cv.convertScaleAbs(img_cv2,.9,5) 
+  img_cv2 = cv.resize(img_cv2, (img_cv.shape[0], img_cv.shape[1]), interpolation = cv.INTER_AREA)
+  print(img_cv.shape)
+  cv.imwrite(f"plots/compare_R.png",img_cv)
+  cv.imwrite(f"plots/compare_IR.png",img_cv2)
+
+
+def find_rectangles(img_cv_in,img_cv_in2):
   #i = 95
-  for i in range(140, 200, 10):
+  img_cv_out = img_cv_in
+  img_cv_out = cv.cvtColor(img_cv_out, cv.COLOR_BGR2GRAY)
+  for i in range(2, 3, 2):
     img_cv = cv.cvtColor(img_cv_in, cv.COLOR_BGR2GRAY)
     #img_cv = img_cv_in
     cv.imwrite("bin_img.png",img_cv)
     #ret, thresh = cv.threshold(img_cv, i, 256,cv.THRESH_OTSU)
     #ret, thresh = cv.threshold(img_cv, i, 256,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
     img_cv = cv.GaussianBlur(img_cv,(5,5), 0)
-    ret, thresh = cv.threshold(img_cv, i, 256,cv.THRESH_BINARY)
     #ret, thresh = cv.threshold(img_cv, i, 256,cv.THRESH_BINARY)
-    #ret, thresh = cv.threshold(img_cv, 105, 256,cv.THRESH_BINARY)
+    thresh = cv.adaptiveThreshold(img_cv,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,13,i)
+    #thresh = cv.adaptiveThreshold(img_cv,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,13,i)
+    #ret, thresh = cv.threshold(img_cv,i,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    #ret, thresh = cv.threshold(img_cv, i, 256,cv.THRESH_BINARY)
     #thresh = cv.GaussianBlur(thresh,(5,5), 0)
     #edge = cv.Canny(thresh, 120, 190, 3)
     #edge = cv.Canny(img_cv, 100, 150, 2)
@@ -84,10 +101,10 @@ def find_rectangles(img_cv_in):
 
     print("Number of contours detected: ", len(contours))
 
-    img_cv2 = cv.imread('Lato_SUD_TIR.tif', cv.IMREAD_UNCHANGED)
-    print(img_cv2.shape[0])
-    print(img_cv.shape[0])
-    img_cv2 = cv.resize(img_cv2, (img_cv.shape[1], img_cv.shape[0]), interpolation = cv.INTER_AREA)
+    #img_cv2 = cv.imread('Lato_SUD_TIR.tif', cv.IMREAD_UNCHANGED)
+    #img_cv2 = cv.imread('1mw_TIR_index_grayscale.tif', cv.IMREAD_UNCHANGED)
+
+    img_cv2 = cv.resize(img_cv_in2, (img_cv.shape[1], img_cv.shape[0]), interpolation = cv.INTER_AREA)
     img_cv2 = cv.cvtColor(np.uint8(img_cv2), cv.COLOR_GRAY2BGR)
     img_cv2 = cv.applyColorMap(img_cv2, cv.COLORMAP_TURBO)
     #img_cv2 = cv.applyColorMap(img_cv2, cv.COLORMAP_JET)
@@ -99,14 +116,8 @@ def find_rectangles(img_cv_in):
       approx = cv.approxPolyDP(cnt, 0.01*cv.arcLength(cnt, True), True)
       if len(approx) == 4:
         #x, y, w, h = cv.boundingRect(cnt)
-        '''
-        img_crop, img_rot = crop_rect(img_cv2,rect)
-        print(type(img_crop))
-        if not(type(img_crop) != 'NoneType'):
-          print("here")
-          cv.imwrite(f"plots/crops/cropped_rect_{k}_param_{i}.png",img_crop)
-        '''
         rect = cv.minAreaRect(cnt)
+        #print(rect)
         box = cv.boxPoints(rect)
         box = np.int0(box)
         x1 = box[0]
@@ -114,25 +125,32 @@ def find_rectangles(img_cv_in):
         x3 = box[2]
         w = np.sum(np.power(x2-x1,2))
         h = np.sum(np.power(x3-x1,2))
+        #print(h)
         #print(f"box: {box}")
-        if( w >30**2 and h > 30**2 and w < 120**2 and h<120**2):
+        if( w >50**2 and h > 50**2 and w < 120**2 and h<120**2):
+          #img_crop, img_rot = crop_rect(img_cv2,rect)
+          #cv.imwrite(f"plots/crops/cropped_rect_{k}_param_{i}.png",img_crop)
           print(f"width: {w}, height: {h}")
           w_arr.append(w)
           h_arr.append(h)
           img_cv = cv.drawContours(img_cv, [cnt], -1, (0, 255,0),8)
           #img_cv2 = cv.drawContours(img_cv2, [cnt], -1, (255, 255,0),8)
-          #img_cv2 = cv.drawContours(img_cv2, [box], -1, (255, 255,0),8)
-          cv.putText(img_cv, f"{k}", (x1[0], x1[1]), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255,0), 2)
+          img_cv_out = cv.drawContours(img_cv_out, [box], -1, (0, 255,0),8)
+          #cv.putText(img_cv, f"{k}", (x1[0], x1[1]), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255,0), 2)
+          #cv.putText(img_cv2, f"{k}", (x1[0], x1[1]), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255,0), 2)
 
     fig, axs =  plt.subplots(1, 2, sharey=True, tight_layout=True)
-    axs[0].hist(w_arr, bins=50)
-    axs[1].hist(h_arr, bins=50)
+    bins_x = range(5500, 7500, 20)
+    bins_y = range(7500, 9500, 20)
+    axs[0].hist(w_arr, bins=bins_x)
+    axs[1].hist(h_arr, bins=bins_y)
     plt.savefig(f"plots/hist/hist_rec_{i}.png")
-    cv.imwrite(f"plots/contours/test_rec_{i}.png",img_cv)
-    #cv.imwrite(f"plots/contours/test_rec_{i}.png",img_cv2)
+    cv.imwrite(f"plots/contours/contour_rec_{i}.png",img_cv)
+    cv.imwrite(f"plots/contours/coutour_IR_rec_{i}.png",img_cv2)
     #cv.imshow("Shapes", img_cv)
     #cv.waitKey(0)
-    cv.destroyAllWindows()
+  cv.imwrite(f"plots/contours/contour_all.png",img_cv_out)
+  cv.destroyAllWindows()
 
 def main():
   Image.MAX_IMAGE_PIXELS = 254162108
@@ -140,23 +158,29 @@ def main():
   #im = Image.open('Lato_SUD.tif')
   im = Image.open('Ortho1mw_Lres.tif')
   im_array = np.array(im)
-  #im_array = im_array * (im_array > 30)
-  print('mean')
-  print(im_array.mean())
-  print('max')
-  print(im_array.max())
-  print('min')
-  print(im_array.mean())
-  print(im.getbands())
-  print(im.tell())
-  print(im_array.shape)
-  print(im_array.size)
   img_cv = im_array.astype(np.uint8)
 
-  find_rectangles(img_cv)
+  im = Image.open('1mw_TIR_index_grayscale.tif')
+  im_array = np.array(im)
+  img_cv2 = im_array.astype(np.uint8)
+
+  #prepare_img(img_cv, img_cv2)
+  find_rectangles(img_cv, img_cv2)
   #plot_preprocessed_img(img_cv)
   #find_circle(img_cv)
 
+  if DEBUG:
+    #im_array = im_array * (im_array > 30)
+    print('mean')
+    print(im_array.mean())
+    print('max')
+    print(im_array.max())
+    print('min')
+    print(im_array.mean())
+    print(im.getbands())
+    print(im.tell())
+    print(im_array.shape)
+    print(im_array.size)
 
 
 if __name__ == '__main__':
